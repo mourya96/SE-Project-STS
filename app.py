f57cdb8edd2014dd8eada308c1a709770e40fce4
Kaustav-Goswami9 <kaustavhome23@gmail.com>
2023-03-11 23:07:33 +0530

    Added server python file

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask
+from flask_restful import Api
+
+from Login_manager_api import Login_api, Role_api
+from model import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.getcwd() + \
+    '/DB_project.sqlite3'
+api = Api(app)
+
+app.config['SECRET_KEY'] = 'FMM-2'
+db.init_app(app)
+app.app_context().push()
+db.create_all()
+
+api.add_resource(Login_api, '/api/register', '/api/login/<string:email>')
+api.add_resource(Role_api, '/api/role', '/api/role/<int:user_id>')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
