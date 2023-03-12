b2108973be5b81ef3f0a0bb49aed802ecbcf739f
Abhishek <lovestory1990@gmail.com>
2023-03-12 18:50:56 +0530

    tag-2 commit

--- Tag_manager_api.py
+++ Tag_manager_api.py
@@ -4,8 +4,7 @@ from model import User,db, Staff, Secondary_Tag,Subject_Tag,Ticket, Table_likes,
 
 from custom_error import DataError, LogicError
 
-tag_output={'tag_id':fields.Integer,
-            'tag_name':fields.String}
+
 
 
 class Tag_api(Resource):
