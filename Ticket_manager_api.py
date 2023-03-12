52bdf8b4323ca2d17d50e179d76d64ad8c5a7f6d
Kaustav-Goswami9 <kaustavhome23@gmail.com>
2023-03-12 17:43:15 +0530

    Removed unused imports

--- Ticket_manager_api.py
+++ Ticket_manager_api.py
@@ -1,92 +1,94 @@
-from flask import request, jsonify
-
-from flask_restful import fields, marshal_with, reqparse, Resource
-from model import User,db, Staff, Secondary_Tag,Subject_Tag,Ticket, Table_likes, Response
+from flask import request
+from flask_restful import Resource, fields, marshal_with
 
 from custom_error import DataError, LogicError
+from model import Secondary_Tag, Subject_Tag, Table_likes, Ticket, db
+
 
 class Ticket_api(Resource):
     '''API code for Ticket Manager'''
 
-    ticket_output={"ticket_id":fields.Integer,
-            "title":fields.String,
-            "description":fields.String,
-            "isFAQ":fields.Boolean,
-            "ticket_status":fields.String,
-            "likes":fields.Integer(attribute=lambda x: len(x.likes))
-            }
+    ticket_output = {"ticket_id": fields.Integer,
+                     "title": fields.String,
+                     "description": fields.String,
+                     "isFAQ": fields.Boolean,
+                     "ticket_status": fields.String,
+                     "likes": fields.Integer(attribute=lambda x: len(x.likes))}
+
     @marshal_with(ticket_output)
-    def put(self,ticket_id:int):
+    def put(self, ticket_id: int):
         '''Modifies the ticket details'''
         ticket_obj = Ticket.query.filter_by(ticket_id=ticket_id).first()
         if not ticket_obj:
             return DataError(status_code=404)
-        
-        form= request.get_json()
-        
-        #Checking action from form data
+
+        form = request.get_json()
+
+        # Checking action from form data
         if form.get("action") is None or form.get("user_id") is None:
             raise LogicError(status_code=400, error_code="TICKET001",
                              error_msg="Either of the form data is missing")
-        
-        user_id=form.get("user_id")
-        
-        #Changes in database based on action variable from form
-        if form.get("action")=='faq':
-            ticket_obj.isFAQ=True
-        
-        elif form.get("action")=='like':
+
+        user_id = form.get("user_id")
+
+        # Changes in database based on action variable from form
+        if form.get("action") == 'faq':
+            ticket_obj.isFAQ = True
+
+        elif form.get("action") == 'like':
             for like in ticket_obj.likes:
-                if like.user_id==user_id:
-                  db.session.delete(like)
-                  break
-            
+                if like.user_id == user_id:
+                    db.session.delete(like)
+                    break
+
             else:
-                obj=Table_likes(ticket_id=ticket_obj.ticket_id,user_id=user_id)
+                obj = Table_likes(
+                    ticket_id=ticket_obj.ticket_id, user_id=user_id)
                 db.session.add(obj)
-        
+
         db.session.commit()
         return ticket_obj, 200
-    
+
     @marshal_with(ticket_output)
-    def post(self,subject_name:str):
+    def post(self, subject_name: str):
         '''Creates a new ticket for a subject'''
-        
-        subject= Subject_Tag.query.filter_by(subject_name=subject_name).first()
+
+        subject = Subject_Tag.query.filter_by(
+            subject_name=subject_name).first()
         if subject is None:
             raise DataError(status_code=404)
-        
-        #Getting form data
-        form = request.get_json()
-        title= form.get("title")
-        desc=form.get("description")
-        sec=form.get("secondary_tag")
-        user_id=form.get("user_id")
-        form_data=[title,desc,sec,user_id]
 
-        #Checking if all the form data is filled up
+        # Getting form data
+        form = request.get_json()
+        title = form.get("title")
+        desc = form.get("description")
+        sec = form.get("secondary_tag")
+        user_id = form.get("user_id")
+        form_data = [title, desc, sec, user_id]
+
+        # Checking if all the form data is filled up
         if None in form_data:
-            raise LogicError(status_code=400,error_code="TICKET002", error_msg="Some form data is missing")
-        
-        #Checking if secondary tag is present in Secondary Tag class
-        sec_obj= Secondary_Tag.query.filter_by(sec_tag_name=sec).first()
+            raise LogicError(status_code=400, error_code="TICKET002",
+                             error_msg="Some form data is missing")
+
+        # Checking if secondary tag is present in Secondary Tag class
+        sec_obj = Secondary_Tag.query.filter_by(sec_tag_name=sec).first()
         if sec_obj is None:
-            raise LogicError(status_code=400,error_code="TICKET003", error_msg="secondary tag selected is not available")
-        
-        #Creating ticket obj and committing to database
-        ticket_obj=Ticket(user_id=user_id,title=title, description=desc, subject_name=subject_name, sec_name=sec)
+            raise LogicError(status_code=400, error_code="TICKET003",
+                             error_msg="secondary tag selected is not available")
+
+        # Creating ticket obj and committing to database
+        ticket_obj = Ticket(user_id=user_id, title=title,
+                            description=desc, subject_name=subject_name, sec_name=sec)
         db.session.add(ticket_obj)
         db.session.commit()
-        return ticket_obj,201
+        return ticket_obj, 201
 
-    def delete(self,ticket_id:int):
+    def delete(self, ticket_id: int):
         '''Deletes the ticket-power given only to admin'''
-        ticket_obj=  Ticket.query.filter_by(ticket_id=ticket_id).first()
+        ticket_obj = Ticket.query.filter_by(ticket_id=ticket_id).first()
         if not ticket_obj:
             raise DataError(status_code=404)
         db.session.delete(ticket_obj)
         db.session.commit()
         return '', 200
-        
-
-
