fa3aec34a3501654b8325d412b707c9e9bcf558b
mourya96 <amcnagesh@gmail.com>
2023-03-18 19:43:19 +0530

    Added Response api resource

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from Login_manager_api import Login_api
 from Role_manager_api import Role_api
 from Tag_manager_api import Tag_api
 from Ticket_manager_api import Ticket_api
+from Response_api_for_TM import Responses_api
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -24,6 +25,9 @@ api.add_resource(Login_api, '/api/register', '/api/login/<string:email>')
 api.add_resource(Role_api, '/api/role', '/api/role/<int:user_id>')
 api.add_resource(Ticket_api, '/api/subject/ticket/<int:ticket_id>',
                  '/api/subject/<string:subject_name>')
+
+api.add_resource(Responses_api, '/api/response', '/api/response/<int:ticket_id>', 
+                 '/api/response/<int:ticket_id>/<int:response_id>')
 
 api.add_resource(Tag_api,
                  '/api/tag/<string:tag_type>', '/api/tag/<string:tag_type>/<int:tag_id>')
