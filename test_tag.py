7eab5f44db2ede77998437e16dac6c57329d83b7
Kaustav-Goswami9 <kaustavhome23@gmail.com>
2023-03-29 02:15:32 +0530

    updated test cases

--- test_tag.py
+++ test_tag.py
@@ -41,9 +41,33 @@ def test_create_secondary_tag(client):
     client.delete(f'/api/tag/secondary/{data["sec_id"]}')
 
 
+def test_edit_subject_tag(client):
+    # Set up test data
+    new_tag = {'tag_name': 'MAD-2'}
+    # Make POST request to create new tag
+    response = client.post('/api/tag/subject', json=new_tag)
+
+    # Check status code of response
+    assert response.status_code == 201
+
+    data = json.loads(response.data)
+    # Set up edit data
+    edit_data = {'tag_name': 'mad-2'}
+    # Make PUT request to edit that tag
+    response = client.put(f'/api/tag/subject/{data["subject_id"]}',
+                          json=edit_data)
+
+    # Check status code of response
+    assert response.status_code == 202
+
+    data = json.loads(response.data)
+    assert data['subject_name'] == 'mad-2'
+    client.delete(f'/api/tag/subject/{data["sec_id"]}')
+
+
 def test_edit_secondary_tag(client):
     # Set up test data
-    new_tag = {'tag_name': 'Quiz-1'}
+    new_tag = {'tag_name': 'Quiz-2'}
     # Make POST request to create new tag
     response = client.post('/api/tag/secondary', json=new_tag)
 
@@ -52,7 +76,7 @@ def test_edit_secondary_tag(client):
 
     data = json.loads(response.data)
     # Set up edit data
-    edit_data = {'tag_name': 'quiz-1'}
+    edit_data = {'tag_name': 'quiz-2'}
     # Make PUT request to edit that tag
     response = client.put(f'/api/tag/secondary/{data["sec_id"]}',
                           json=edit_data)
@@ -61,5 +85,5 @@ def test_edit_secondary_tag(client):
     assert response.status_code == 202
 
     data = json.loads(response.data)
-    assert data['sec_name'] == 'quiz-1'
+    assert data['sec_name'] == 'quiz-2'
     client.delete(f'/api/tag/secondary/{data["sec_id"]}')
