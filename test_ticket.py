92f1d16350f0cfbb5e81e79583f9d987b52528c9
Abhishek <lovestory1990@gmail.com>
2023-03-30 09:59:29 +0530

    test_ticket update

---
+++ test_ticket.py
@@ -0,0 +1,55 @@
+
+import json
+
+
+def test_create_ticket(client):
+    # Set up test data
+    new_ticket = {
+        "title": "title-ba",
+        "description": "desc-ba",
+        "secondary_tag": "week-1",
+        "user_id": 1
+    }
+    tag_name = "BA"
+    # Make POST request to create new ticket
+    response = client.post(f'/api/subject/{tag_name}', json=new_ticket)
+
+    # Check status code of response
+    assert response.status_code == 201
+
+    # Check response body for correct username
+    ticket_obj = json.loads(response.data)
+    ticket_id = ticket_obj['ticket_id']
+    client.delete(f'/api/subject/ticket/{ticket_id}')
+    assert ticket_obj['title'] == 'title-ba'
+
+
+def test_mark_resolved_ticket_as_faq(client):
+    # Preconditions-corresponding to ticket_id=1, the ticket_Status should be resolved. user_id =4 should also correspond to staff
+    action_detail = {
+        "action": "faq",
+        "user_id": 4
+    }
+
+    ticket_id = 1
+    response_id = 1
+    response = client.put(
+        f'/api/subject/ticket/{ticket_id}', json=action_detail)
+    ticket_obj = json.loads(response.data)
+    assert response.status_code == 200
+    assert ticket_obj['isFAQ'] == True
+
+
+def test_mark_unresolved_ticket_as_faq(client):
+    # Preconditions-corresponding to ticket_id=2, the ticket_Status should be unresolved. user_id =4 should also correspond to staff
+    action_detail = {
+        "action": "faq",
+        "user_id": 4
+    }
+
+    ticket_id = 2
+    response = client.put(
+        f'/api/subject/ticket/{ticket_id}', json=action_detail)
+    error = json.loads(response.data)['error_code']
+    assert response.status_code == 400
+    assert error == "TICKET002"
