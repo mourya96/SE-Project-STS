5a5b3be27830618ea161cb3281fa1e072bc3d377
Abhishek <lovestory1990@gmail.com>
2023-04-04 20:23:46 +0530

    Changes in Tests

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,27 +1,29 @@
 import json
+from model import Response, Ticket, db
 from pprint import pprint
 
 
 def test_get_response(client):
-    url = "http://127.0.0.1:5500/api/response/1"
+
+    url = "/api/response/1"
     res = client.get(url)
     json_data = json.loads(res.data)
     pprint(json_data)
     assert res.status_code == 200
-    assert json_data['ticket_id'] == 1
+    assert len(json_data['response_list']) == 4
 
 
 def test_get_response_error(client):
-    url = "http://127.0.0.1:5500/api/response/9999"
+    url = "/api/response/9999"
     res = client.get(url)
     json_data = json.loads(res.data)
-    pprint(json_data)
+    # pprint(json_data)
     assert res.status_code == 404
     assert json_data['error_code'] == 'RESPONSE001'
 
 
 def test_post_response(client):
-    url = "http://127.0.0.1:5500/api/response/1"
+    url = "/api/response/1"
     payload = {
         "user_id": 1,
         "response": "Test Response"
@@ -30,11 +32,14 @@ def test_post_response(client):
     json_data = json.loads(res.data)
     assert res.status_code == 201
     assert json_data['ticket_id'] == 1
+    obj = Response.query.filter_by(response=payload['response']).first()
+    db.session.delete(obj)
+    db.session.commit()
 
 
 def test_post_response_error(client):
     # 404 status code test
-    url = "http://127.0.0.1:5500/api/response/9999"
+    url = "/api/response/9999"
     payload = {
         "user_id": 1,
         "response": "Test Response"
@@ -44,7 +49,7 @@ def test_post_response_error(client):
     assert res.status_code == 404
     assert json_data['error_code'] == 'RESPONSE001'
     # 400 status code test
-    url = "http://127.0.0.1:5500/api/response/1"
+    url = "/api/response/1"
     payload = {
         "response": "Test Response"
     }
@@ -53,7 +58,7 @@ def test_post_response_error(client):
     assert res.status_code == 400
     assert json_data['error_code'] == 'RESPONSE003'
 
-    url = "http://127.0.0.1:5500/api/response/1"
+    url = "/api/response/1"
     payload = {
         "user_id": 1
     }
@@ -64,7 +69,7 @@ def test_post_response_error(client):
 
 
 def test_put_response(client):
-    url = "http://127.0.0.1:5500/api/response/1/4"
+    url = "/api/response/1/4"
     payload = {
         "isAnswer": True,
         "ticket_status": "resolved"
@@ -75,7 +80,7 @@ def test_put_response(client):
 
 
 def test_put_response_error_1(client):
-    url = "http://127.0.0.1:5500/api/response/9999/4"
+    url = "/api/response/9999/4"
     payload = {
         "isAnswer": False,
         "ticket_status": "unresolved"
@@ -87,7 +92,7 @@ def test_put_response_error_1(client):
 
 
 def test_put_response_error_2(client):
-    url = "http://127.0.0.1:5500/api/response/1/999"
+    url = "/api/response/1/999"
     payload = {
         "isAnswer": False,
         "ticket_status": "unresolved"
@@ -99,7 +104,7 @@ def test_put_response_error_2(client):
 
 
 def test_put_response_error_3(client):
-    url = "http://127.0.0.1:5500/api/response/1/4"
+    url = "/api/response/1/4"
     payload = {
         "ticket_status": "resolved"
     }
@@ -110,7 +115,7 @@ def test_put_response_error_3(client):
 
 
 def test_put_response_error_4(client):
-    url = "http://127.0.0.1:5500/api/response/1/4"
+    url = "/api/response/1/4"
     payload = {
         "isAnswer": False,
     }
@@ -121,7 +126,7 @@ def test_put_response_error_4(client):
 
 
 def test_put_response_error_5(client):
-    url = "http://127.0.0.1:5500/api/response/1/4"
+    url = "/api/response/1/4"
     payload = {
         "isAnswer": False,
         "ticket_status": "hello"
