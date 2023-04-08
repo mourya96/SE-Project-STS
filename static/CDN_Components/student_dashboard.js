bd06e3f20fcf5c3d413841190af8750ff37bf36c
Kaustav-Goswami9 <kaustavhome23@gmail.com>
2023-04-09 00:33:29 +0530

    Student and Subject Dashboard updated.

--- static/CDN_Components/student_dashboard.js
+++ static/CDN_Components/student_dashboard.js
@@ -1,30 +1,27 @@
+import navBar from "./navBar.js"
 const student_dashboard = {
     template: `
     <div>
         <link rel="stylesheet" href="../static/dashboard.css">
         
-        <h1 class="text-center">Student Dashboard</h1>
-        <div class="position-relative" style="margin-left: 3%; margin-right: 3%;">
-            <h3 class=" ms-3">Welcome {{username}}</h3>
-            <div class="text-end me-1 position-absolute top-0 end-0">
-                <button @click="logout" style="font-size: large;" class="btn btn-info">
-                    Logout <i class="bi bi-box-arrow-right"></i>
-                </button>
-            </div>
-            <hr>
-        </div>
+        <navB :title="title"></navB>
         <div v-if="!ready">
             <h1>Please wait Loading</h1>
         </div>
         <div class="row" v-else>
             <div class="col-sm-4" v-for="(faq, subject) in subjects" :key="subject">
-                <div class="card position-relative " style="margin-left: 5%; margin-right: 5%;">
-                    <div class="card-body dark-mode">
-                        <h3 class="card-title text-center">{{subject}}</h3>
+                <div class="card" style="margin-left: 5%; margin-right: 5%;">
+                    <div class="card-body ">
+                        <router-link :to="'/subject/'+subject"><h3 class="card-title text-center">{{subject}}</h3></router-link>
                         <hr class="ms-3">
-                        <ul v-for="title in faq" :key="title">
-                            <li class="card-title">{{title}}</li>
-                        </ul>
+                        <div v-if="faq.length == 0" class="text-center">
+                            <router-link :to="'/subject/'+subject">Click Here to view all the tickets of this subject</router-link>
+                        </div>
+                        <div v-else>
+                            <ul v-for="ticket in faq" :key="title">
+                                <router-link :to="'/ticket/'+ticket.ticket_id"> <li class="card-title">{{ticket.title}}</li></router-link>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -33,16 +30,16 @@ const student_dashboard = {
     data() {
         return {
             username: 'student',
+            title: "Student Dashboard",
             subjects: {},
             ready: false,
             darkMode: false
         }
     },
+    components: {
+        'navB': navBar
+    },
     methods: {
-        logout() {
-            alert("Logout Successfully")
-            localStorage.clear()
-        }
     },
     beforeMount() {
         fetch('/api/tag/subject')
@@ -53,11 +50,17 @@ const student_dashboard = {
                 for (const subject of subject_names) {
                     fetch(`/api/subject/${subject}?FAQ=True&limit=5`)
                         .then(res => res.json())
-                        .then(data => this.subjects[subject] = data.map(x => x.title))
+                        .then(data => this.subjects[subject] = data)
                 }
             }).catch(err => console.log(err))
         this.ready = true
-    }
+    },
+    // beforeCreate() {
+    //     if (!localStorage.getItem('access-token')) {
+    //         alert('plz login first')
+    //         // return this.$router.push('/login')
+    //     }
+    // }
 }
 
 export default student_dashboard
